460fd854abb6e92c40ee668e1fb2c54ecaaf37aa
Lilach <Lilach.Davis@cps.gov.uk>
2026-03-11 16:03:42 +0000

    comment out prod (#37)

--- environments/prod/applications/alerts.tf
+++ environments/prod/applications/alerts.tf
@@ -1,28 +1,28 @@
-module "alert_api_5xx" {
-  source = "../../../modules/alert-fa-5xx"
+# module "alert_api_5xx" {
+#   source = "../../../modules/alert-fa-5xx"
 
-  environment     = var.environment
-  project_acronym = var.project_acronym
-  location        = var.location
-  tags            = local.tags
-  rg_name         = module.rg.rg_name
+#   environment     = var.environment
+#   project_acronym = var.project_acronym
+#   location        = var.location
+#   tags            = local.tags
+#   rg_name         = module.rg.rg_name
 
-  functional_area  = "api"
-  app_insights_id  = module.ai.ai_id
-  action_group_ids = [module.ag_api_alerts.id]
-  fa_name          = module.fa_main.fa_name
-}
+#   functional_area  = "api"
+#   app_insights_id  = module.ai.ai_id
+#   action_group_ids = [module.ag_api_alerts.id]
+#   fa_name          = module.fa_main.fa_name
+# }
 
-module "alert_api_outage" {
-  source = "../../../modules/alert-app-outage"
+# module "alert_api_outage" {
+#   source = "../../../modules/alert-app-outage"
 
-  environment     = var.environment
-  project_acronym = var.project_acronym
-  tags            = local.tags
-  rg_name         = module.rg.rg_name
+#   environment     = var.environment
+#   project_acronym = var.project_acronym
+#   tags            = local.tags
+#   rg_name         = module.rg.rg_name
 
-  functional_area = "api"
-  app_id          = module.fa_main.fa_id
-  app_name        = module.fa_main.fa_name
-  action_group_id = module.ag_api_alerts.id
-}
+#   functional_area = "api"
+#   app_id          = module.fa_main.fa_id
+#   app_name        = module.fa_main.fa_name
+#   action_group_id = module.ag_api_alerts.id
+# }
--- environments/prod/applications/data.tf
+++ environments/prod/applications/data.tf
@@ -1,32 +1,32 @@
-data "azurerm_client_config" "current" {}
+# data "azurerm_client_config" "current" {}
 
-# The Enterprise App (service principal) used to deploy code to the resources in the environment
-data "azuread_service_principal" "ado" {
-  display_name = var.aad_sp_name
-}
+# # The Enterprise App (service principal) used to deploy code to the resources in the environment
+# data "azuread_service_principal" "ado" {
+#   display_name = var.aad_sp_name
+# }
 
-data "azurerm_private_dns_zone" "dns" {
-  for_each = {
-    blob  = "privatelink.blob.core.windows.net"
-    sites = "privatelink.azurewebsites.net"
-    vault = "privatelink.vaultcore.azure.net"
-  }
+# data "azurerm_private_dns_zone" "dns" {
+#   for_each = {
+#     blob  = "privatelink.blob.core.windows.net"
+#     sites = "privatelink.azurewebsites.net"
+#     vault = "privatelink.vaultcore.azure.net"
+#   }
 
-  name                = each.value
-  resource_group_name = var.vnet_rg
-}
+#   name                = each.value
+#   resource_group_name = var.vnet_rg
+# }
 
-data "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  resource_group_name = var.vnet_rg
-}
+# data "azurerm_virtual_network" "vnet" {
+#   name                = var.vnet_name
+#   resource_group_name = var.vnet_rg
+# }
 
-# All subnets of the vnet above.
-# To reference a specific subnet use data.azurerm_subnet.base["<subnet-name>"].id
-data "azurerm_subnet" "base" {
-  for_each = toset(data.azurerm_virtual_network.vnet.subnets)
+# # All subnets of the vnet above.
+# # To reference a specific subnet use data.azurerm_subnet.base["<subnet-name>"].id
+# data "azurerm_subnet" "base" {
+#   for_each = toset(data.azurerm_virtual_network.vnet.subnets)
 
-  name                 = each.value
-  virtual_network_name = var.vnet_name
-  resource_group_name  = var.vnet_rg
-}
+#   name                 = each.value
+#   virtual_network_name = var.vnet_name
+#   resource_group_name  = var.vnet_rg
+# }
