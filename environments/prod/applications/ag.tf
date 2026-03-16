cfb0e25af4442d8ca5e2ec3d582fd5156bfda4d2
Lilach <Lilach.Davis@cps.gov.uk>
2026-03-16 09:45:00 +0000

    Revert "comment out prod (#37)"
    This reverts commit 460fd854abb6e92c40ee668e1fb2c54ecaaf37aa.

--- environments/prod/applications/ag.tf
+++ environments/prod/applications/ag.tf
@@ -1,18 +1,18 @@
-# module "ag_api_alerts" {
-#   source = "../../../modules/action-group"
+module "ag_api_alerts" {
+  source = "../../../modules/action-group"
 
-#   environment     = var.environment
-#   project_acronym = var.project_acronym
-#   tags            = local.tags
-#   rg_name         = module.rg.rg_name
+  environment     = var.environment
+  project_acronym = var.project_acronym
+  tags            = local.tags
+  rg_name         = module.rg.rg_name
 
-#   function   = "api"
-#   short_name = "${var.project_acronym}-api"
+  function   = "api"
+  short_name = "${var.project_acronym}-api"
 
-#   email_receivers = {
-#     EmailDevTeam = {
-#       email                   = var.dev_team_email
-#       use_common_alert_schema = false
-#     }
-#   }
-# }
+  email_receivers = {
+    EmailDevTeam = {
+      email                   = var.dev_team_email
+      use_common_alert_schema = false
+    }
+  }
+}
