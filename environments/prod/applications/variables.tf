ab7916cd665641f6dd31d6d5e1c382fcfb7568d6
Lilach <Lilach.Davis@cps.gov.uk>
2026-03-18 17:15:11 +0000

    take down app layer resources

--- environments/prod/applications/variables.tf
+++ environments/prod/applications/variables.tf
@@ -1,17 +1,17 @@
-# variable "project_acronym" {
-#   type        = string
-#   description = "The abbreviated project name."
-# }
+variable "project_acronym" {
+  type        = string
+  description = "The abbreviated project name."
+}
 
-# variable "environment" {
-#   type        = string
-#   description = "The deployment environment."
-# }
+variable "environment" {
+  type        = string
+  description = "The deployment environment."
+}
 
-# variable "location" {
-#   type        = string
-#   description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created"
-# }
+variable "location" {
+  type        = string
+  description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created"
+}
 
 # variable "vnet_name" {
 #   type        = string
