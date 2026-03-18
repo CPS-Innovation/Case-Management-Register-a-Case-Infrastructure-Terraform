7eb77a0c313a1cac36533aef1f6b3fe7d62fe0f9
Lilach <Lilach.Davis@cps.gov.uk>
2026-03-18 17:07:07 +0000

    Take down Prod app layer resources (#38)

--- environments/prod/applications/variables.tf
+++ environments/prod/applications/variables.tf
@@ -1,45 +1,45 @@
-variable "project_acronym" {
-  type        = string
-  description = "The abbreviated project name."
-}
+# variable "project_acronym" {
+#   type        = string
+#   description = "The abbreviated project name."
+# }
 
-variable "environment" {
-  type        = string
-  description = "The deployment environment."
-}
+# variable "environment" {
+#   type        = string
+#   description = "The deployment environment."
+# }
 
-variable "location" {
-  type        = string
-  description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created"
-}
+# variable "location" {
+#   type        = string
+#   description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created"
+# }
 
-variable "vnet_name" {
-  type        = string
-  description = "The name of the virtual network in which to create the subnet"
-}
+# variable "vnet_name" {
+#   type        = string
+#   description = "The name of the virtual network in which to create the subnet"
+# }
 
-variable "vnet_rg" {
-  type        = string
-  description = "The name of the virtual network in which to create the subnet"
-}
+# variable "vnet_rg" {
+#   type        = string
+#   description = "The name of the virtual network in which to create the subnet"
+# }
 
-variable "aad_sp_name" {
-  type        = string
-  description = "Display name of the enterprise app (service principal) used to deploy code to the resources in the environment"
-}
+# variable "aad_sp_name" {
+#   type        = string
+#   description = "Display name of the enterprise app (service principal) used to deploy code to the resources in the environment"
+# }
 
-variable "asp_linux_sku" {
-  type        = string
-  description = "The SKU of the Linux app service plan."
-}
+# variable "asp_linux_sku" {
+#   type        = string
+#   description = "The SKU of the Linux app service plan."
+# }
 
-variable "asp_windows_sku" {
-  type        = string
-  description = "The SKU of the Windows app service plan."
-}
+# variable "asp_windows_sku" {
+#   type        = string
+#   description = "The SKU of the Windows app service plan."
+# }
 
-variable "dev_team_email" {
-  type        = string
-  description = "The DL email address of the project's dev team."
-  sensitive   = true
-}
+# variable "dev_team_email" {
+#   type        = string
+#   description = "The DL email address of the project's dev team."
+#   sensitive   = true
+# }
