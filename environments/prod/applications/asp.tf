f3485ad921c742446c6696290d278f229373b45c
Lilach <Lilach.Davis@cps.gov.uk>
2026-03-18 16:50:02 +0000

    take down app layer resources

--- environments/prod/applications/asp.tf
+++ environments/prod/applications/asp.tf
@@ -1,25 +1,25 @@
-module "asp_linux" {
-  source = "../../../modules/app-service-plan"
+# module "asp_linux" {
+#   source = "../../../modules/app-service-plan"
 
-  environment     = var.environment
-  project_acronym = var.project_acronym
-  location        = var.location
-  tags            = local.tags
-  rg_name         = module.rg.rg_name
+#   environment     = var.environment
+#   project_acronym = var.project_acronym
+#   location        = var.location
+#   tags            = local.tags
+#   rg_name         = module.rg.rg_name
 
-  os_type = "linux"
-  sku     = var.asp_linux_sku
-}
+#   os_type = "linux"
+#   sku     = var.asp_linux_sku
+# }
 
-module "asp_windows" {
-  source = "../../../modules/app-service-plan"
+# module "asp_windows" {
+#   source = "../../../modules/app-service-plan"
 
-  environment     = var.environment
-  project_acronym = var.project_acronym
-  location        = var.location
-  tags            = local.tags
-  rg_name         = module.rg.rg_name
+#   environment     = var.environment
+#   project_acronym = var.project_acronym
+#   location        = var.location
+#   tags            = local.tags
+#   rg_name         = module.rg.rg_name
 
-  os_type = "windows"
-  sku     = var.asp_windows_sku
-}
+#   os_type = "windows"
+#   sku     = var.asp_windows_sku
+# }
