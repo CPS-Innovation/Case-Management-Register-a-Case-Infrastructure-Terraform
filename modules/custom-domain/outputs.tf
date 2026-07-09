aaa68029f91154d3868d9c9b49a671f734bb851a
Lilach <Lilach.Davis@cps.gov.uk>
2026-07-09 15:44:09 +0100

    update cors allowed origins for Prod backend (#95)

--- modules/custom-domain/outputs.tf
+++ modules/custom-domain/outputs.tf
@@ -5,3 +5,7 @@ output "hostname_binding_id" {
 output "certificate_binding_id" {
   value = azurerm_app_service_certificate_binding.hostname.id
 }
+
+output "hostname" {
+  value = azurerm_app_service_custom_hostname_binding.hostname.hostname
+}
